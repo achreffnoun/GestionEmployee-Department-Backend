976846174d37a2eb4773734033e74c38c80a26b1
achreffnoun <achreffnoun@gmail.com>
2022-12-04 14:01:38 +0100

    Update department added

--- src/main/java/com/gestionemp/demo/controllers/DepartmentController.java
+++ src/main/java/com/gestionemp/demo/controllers/DepartmentController.java
@@ -1,7 +1,9 @@
 package com.gestionemp.demo.controllers;
 
 import com.gestionemp.demo.entities.Department;
+import com.gestionemp.demo.entities.Employee;
 import com.gestionemp.demo.models.DepartmentModel;
+import com.gestionemp.demo.models.EmployeeModel;
 import com.gestionemp.demo.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -31,6 +34,18 @@ public class DepartmentController {
 
     }
 
+    @PutMapping("/departments/{id}")
+    ResponseEntity UpdateDepartment(@RequestBody(required = false) DepartmentModel d, @PathVariable long id){
+        if(d == null){
+            return new ResponseEntity("Department not found!",HttpStatus.BAD_REQUEST);
+        }
+        Optional<Department> dep = Optional.ofNullable(this.depServ.getOneDepartment(id));
+        Department dep1 = dep.get();
+        dep1.setName(d.getName());
+        this.depServ.addDepartment(dep1);
+        return new ResponseEntity("Department Updated", HttpStatus.CREATED);
+
+    }
 
     @GetMapping("/departments")
     List<Department> getAllDepartment(){
